b07e5a5fdb165759e85030204cdea7f7b294308e
George Mikheev <GeorgeMikheev56@yandex.ru>
2024-08-22 22:38:59 +0300

    the function of opening a modal window is implemented

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,15 +5,17 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export const ArrowButton = ({open, isOpen}: any) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={open}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={isOpen ? `${styles.container_open} ${styles.container}` : styles.container}
+		>
+			<img src={arrow} alt='иконка стрелочки' className={isOpen ? `${styles.arrow_open} ${styles.arrow}` : styles.arrow} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,18 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { useState } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [asideIsOpen, setAsideOpen] = useState(false);
+
+	const openAside = () => setAsideOpen(prevState => !prevState);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton open={openAside} isOpen={asideIsOpen} />
+			<aside className={asideIsOpen ? `${styles.container_open} ${styles.container}` : styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
